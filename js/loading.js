f2fdc7111034c7b377922bb98558ab65abb7f786
Alliana Hinson-Taylor <142771961+Allihinsontaylor@users.noreply.github.com>
2024-03-15 16:26:55 +0000

    Fri, Mar 15, 2024, 9:26 AM -07:00

--- js/loading.js
+++ js/loading.js
@@ -7,12 +7,14 @@ int = setInterval(blurring, 30)
 
 
 function blurring() {
-    load = load + 1
+    load++
 
     if (load > 99){
         clearInterval(int)
     }
     loadText.innerText = ${load}%
+
+    
     loadText.style.opacity = 1 - load/100
     bg.style.filter = "blur(${30-(load/100)*30}px)"
 }
